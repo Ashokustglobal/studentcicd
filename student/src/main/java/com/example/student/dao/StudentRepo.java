db15a01bb47f99e63adc8e38dfee99f60d1ec93e
Ashokustglobal <121092412+Ashokustglobal@users.noreply.github.com>
2023-01-20 14:23:48 +0530

    Delete student directory

--- student/src/main/java/com/example/student/dao/StudentRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.student.dao;
-
-import com.example.student.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepo extends JpaRepository<Student,Integer> {
-}
